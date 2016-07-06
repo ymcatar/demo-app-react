7382f8b55c6c7845cdedf45457bc3ea12682bb7d
ymcatar <ymcatar@gmail.com>
2016-07-06 15:23:42 +0800

    add: data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    topic: 'Intro to Flux and Redux',
+    speaker: 'Joseph'
+  },
+  {
+    topic: 'OneSky\'s take on using redux with AngularJs',
+    speaker: 'Roger'
+  },
+  {
+    topic: 'Why Functional Programming is so good with Scala?',
+    speaker: 'Cherry'
+  },
+  {
+    topic: 'Stuff at our Startup powered by Scala',
+    speaker: 'Jimmy'
+  },
+  {
+    topic: 'Intro to AWS Lambda, when to use?',
+    speaker: 'Vincent'
+  },
+  {
+    topic: 'Managing Lambda with Apex',
+    speaker: 'Roger'
+  },
+  {
+    topic: 'Angular2 vs ReactJS',
+    speaker: 'Jason'
+  }
+];
